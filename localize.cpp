e2ed6d9495809508068f3a8d3a1e62bc88003485
Gilmar <gilmar.silva@ccc.ufcg.edu.br>
2019-04-18 12:38:23 -0300

    usuario pode digitar a opcao desejada

--- localize.cpp
+++ localize.cpp
@@ -1,4 +1,4 @@
-
+﻿
 
 #include <iostream>
 #include <stdio.h>
@@ -27,18 +27,39 @@ void apresentacao(){
 
 int main()
 {
-		std::cout << "██████╗ ███████╗███╗   ███╗    ██╗   ██╗██╗███╗   ██╗██████╗  ██████╗ \n";
+	while(true){
+	std::cout << "██████╗ ███████╗███╗   ███╗    ██╗   ██╗██╗███╗   ██╗██████╗  ██████╗ \n";
         std::cout << "██╔══██╗██╔════╝████╗ ████║    ██║   ██║██║████╗  ██║██╔══██╗██╔═══██╗\n";
         std::cout << "██████╔╝█████╗  ██╔████╔██║    ██║   ██║██║██╔██╗ ██║██║  ██║██║   ██║\n";
         std::cout << "██╔══██╗██╔══╝  ██║╚██╔╝██║    ╚██╗ ██╔╝██║██║╚██╗██║██║  ██║██║   ██║\n";
         std::cout << "██████╔╝███████╗██║ ╚═╝ ██║     ╚████╔╝ ██║██║ ╚████║██████╔╝╚██████╔╝\n";
-		std::cout << "╚═════╝ ╚══════╝╚═╝     ╚═╝      ╚═══╝  ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝ \n";
+	std::cout << "╚═════╝ ╚══════╝╚═╝     ╚═╝      ╚═══╝  ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝ \n";
         std::cout << "À LOCALIZE CAR\n";
         std::cout << "COMO VOCÊ DESEJA SEGUIR?\n";
         std::cout << "(1) PESQUISAR/ALUGAR\n";
         std::cout << "(2) DEVOLVER\n";
         std::cout << "(3) SAIR\n";
+	int opcao;
 
+        cin >> opcao;
+
+        if (opcao == 3)
+        {
+            break;
+        }
+        else if (opcao == 1)
+        {
+            break;
+        }
+        else if (opcao == 2)
+        {
+           break;
+        }
+        else
+        {
+            cout << "SELECIONE UMA OPÇAO VALIDA!" << endl;
+        }
+    }
 
 
 return 0;
